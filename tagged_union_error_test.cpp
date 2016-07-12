daca3ff454ee3c759c124a0a37a6846c75e4a447
Matt Donovan <mkd1219@gmail.com>
2016-07-11 20:36:12 -0400

    Added performance tests, and moved code to separate files

---
+++ tagged_union_error_test.cpp
@@ -0,0 +1,141 @@
+#include "tagged_union.h"
+#include <cassert>
+#include <string>
+#include <iostream>
+
+static int Foo_count = 0;
+struct Foo {
+  Foo(int a, int b) : a{a}, b{b} { ++Foo_count; }
+  Foo(const Foo &f) : a{f.a}, b{f.b} 
+  { 
+    if (a == -1 && b == -1) {
+      throw std::runtime_error("Foo ctor exception"); 
+    }
+    ++Foo_count;
+  }
+  ~Foo() { --Foo_count; }
+  int a, b;
+};
+
+enum TypeResult {
+  IntType,
+  CharType,
+  DoubleType,
+  FooType,
+  StringType
+};
+
+struct Func {
+  TypeResult operator()(int n) { return IntType; }
+  TypeResult operator()(char c) { return CharType; }
+  TypeResult operator()(double d) { return DoubleType; }
+  TypeResult operator()(Foo &f) { return FooType; }
+  TypeResult operator()(std::string &s) { return StringType; }
+};
+
+void errorTest()
+{
+  using std::string;
+  TaggedUnion<char, double, Foo, string, int> u(8.5);
+
+  size_t max_align = alignof(char);
+  if (max_align < alignof(double)) max_align = alignof(double);
+  if (max_align < alignof(Foo)) max_align = alignof(Foo);
+  if (max_align < alignof(string)) max_align = alignof(string);
+  if (max_align < alignof(int)) max_align = alignof(int);
+  assert(max_align == alignof(decltype(u)));
+  assert(sizeof(u) % max_align == 0);
+
+  size_t max_size = sizeof(char);
+  if (max_size < sizeof(double)) max_size = sizeof(double);
+  if (max_size < sizeof(Foo)) max_size = sizeof(Foo);
+  if (max_size < sizeof(string)) max_size = sizeof(string);
+  if (max_size < sizeof(int)) max_size = sizeof(int);
+  size_t expected_size = max_size + sizeof(int);
+  if (expected_size % max_align != 0) {
+    expected_size += max_align - expected_size % max_align;
+  }
+  assert(expected_size == sizeof(u));
+
+
+  // u.reset(short(1)); // compile error
+  // u.get<short>() = 7; // exception thrown
+  // u.unsafeGet<short>() = 7; // undefined behavior
+
+  assert(u.call(Func()) == DoubleType);
+  assert(u.isType<double>());
+  assert(u.get<double>() == 8.5);
+  assert(u.unsafeGet<double>() == 8.5);
+
+  u.reset(5);
+  assert(u.call(Func()) == IntType);
+  assert(u.isType<int>());
+  assert(u.get<int>() == 5);
+  assert(u.unsafeGet<int>() == 5);
+
+  assert(Foo_count == 0);
+  u.reset(Foo(2, 3));
+  assert(Foo_count == 1); // incremented in Foo ctor
+  assert(u.call(Func()) == FooType);
+  assert(u.isType<Foo>());
+  assert(u.get<Foo>().a == 2 && u.get<Foo>().b == 3);
+  assert(u.unsafeGet<Foo>().a == 2 && u.unsafeGet<Foo>().b == 3);
+
+
+  u.reset(string("hello world"));
+  assert(Foo_count == 0); // decremented in Foo dtor
+  assert(u.call(Func()) == StringType);
+  assert(u.isType<string>());
+  assert(u.get<string>() == string("hello world"));
+  assert(u.unsafeGet<string>() == string("hello world"));
+
+  //TaggedUnion<char, double, Foo, string, int> u2(std::move(u));
+  auto u2 = std::move(u);
+  assert(u.call(Func()) == StringType);
+  assert(u.isType<string>());
+  assert(u.get<string>() == string());
+  assert(u.unsafeGet<string>() == string());
+  assert(u2.call(Func()) == StringType);
+  assert(u2.isType<string>());
+  assert(u2.get<string>() == string("hello world"));
+  assert(u2.unsafeGet<string>() == string("hello world"));
+
+  u = std::move(u2);
+  assert(u.call(Func()) == StringType);
+  assert(u.isType<string>());
+  assert(u.get<string>() == string("hello world"));
+  assert(u.unsafeGet<string>() == string("hello world"));
+  assert(u2.call(Func()) == StringType);
+  assert(u2.isType<string>());
+  assert(u2.get<string>() == string());
+  assert(u2.unsafeGet<string>() == string());
+
+  u2 = u;
+  assert(u.call(Func()) == StringType);
+  assert(u.isType<string>());
+  assert(u.get<string>() == string("hello world"));
+  assert(u.unsafeGet<string>() == string("hello world"));
+  assert(u2.call(Func()) == StringType);
+  assert(u2.isType<string>());
+  assert(u2.get<string>() == string("hello world"));
+  assert(u2.unsafeGet<string>() == string("hello world"));
+
+  try {
+    u.get<int>() = 0; // will throw
+    assert(false);
+  } catch (...) {  }
+
+  try {
+    // exception in copy ctor, leaving u in an invalid state
+    u.reset(Foo(-1,-1)); 
+    assert(false);
+  } catch (...) {  }
+
+  try {
+    u.call(Func()); // trying to call in an invalid state, so will throw
+    assert(false);
+  } catch (...) {  }
+
+  std::cout << "All error tests passed\n";
+}
+
---
+++ tagged_union_perf_test.cpp
@@ -0,0 +1,192 @@
+#include "tagged_union.h"
+#include <iostream>
+#include <chrono>
+#include <vector>
+
+
+struct UnionA {
+  int foo(int n) { return n + 1; }
+};
+struct UnionB {
+  int foo(int n) { return n * 2; }
+};
+struct UnionC {
+  int foo(int n) { return n / 2; }
+};
+
+struct FuncFoo {
+  FuncFoo(int n) : n{n} { }
+  int operator()(UnionA *u) { return u->foo(n); }
+  int operator()(UnionB *u) { return u->foo(n); }
+  int operator()(UnionC *u) { return u->foo(n); }
+  int n;
+};
+
+struct FuncDelete {
+  void operator()(UnionA *u) { delete u; }
+  void operator()(UnionB *u) { delete u; }
+  void operator()(UnionC *u) { delete u; }
+};
+
+void taggedUnionPerfTest(int num_objects)
+{
+  using std::vector;
+  using std::cout;
+  using std::endl;
+  typedef TaggedUnion<UnionA*, UnionB*, UnionC*> UnionT;
+  std::vector<UnionT> unions;
+
+  for (int i = 0; i < num_objects; ++i) {
+    if (i % 3 == 0) {
+      unions.push_back(UnionT(new UnionA())); 
+    } else if (i % 3 == 1) {
+      unions.push_back(UnionT(new UnionB())); 
+    } else {
+      unions.push_back(UnionT(new UnionC())); 
+    }
+  }
+
+  auto t1 = std::chrono::high_resolution_clock::now();
+
+  int n = 1;
+  for (UnionT &u : unions) {
+    n += u.call(FuncFoo(n));
+  }
+
+  auto t2 = std::chrono::high_resolution_clock::now();
+
+  for (UnionT &u : unions) {
+    u.call(FuncDelete());
+  }
+  std::chrono::duration<double, std::milli> duration = t2 - t1;
+  cout << "taggedUnionPerfTest: " << duration.count() << "ms\n"; 
+}
+
+enum UnionTag {
+  UnionAType,
+  UnionBType,
+  UnionCType
+};
+
+struct UnsafeUnion {
+  UnionTag tag;
+  union {
+    UnionA *a;
+    UnionB *b;
+    UnionC *c;
+  };
+};
+
+void unsafeUnionPerfTest(int num_objects)
+{
+  using std::vector;
+  using std::cout;
+  using std::endl;
+  std::vector<UnsafeUnion> unions;
+
+  for (int i = 0; i < num_objects; ++i) {
+    if (i % 3 == 0) {
+      UnsafeUnion u;
+      u.tag = UnionAType;
+      u.a = new UnionA();
+      unions.push_back(u); 
+    } else if (i % 3 == 1) {
+      UnsafeUnion u;
+      u.tag = UnionBType;
+      u.b = new UnionB();
+      unions.push_back(u); 
+    } else {
+      UnsafeUnion u;
+      u.tag = UnionCType;
+      u.c = new UnionC();
+      unions.push_back(u); 
+    }
+  }
+
+  auto t1 = std::chrono::high_resolution_clock::now();
+
+  int n = 1;
+  for (UnsafeUnion &u : unions) {
+    switch (u.tag) {
+    case UnionAType:
+      n += u.a->foo(n);
+      break;
+    case UnionBType:
+      n += u.b->foo(n);
+      break;
+    case UnionCType:
+      n += u.c->foo(n);
+      break;
+    }
+  }
+
+  auto t2 = std::chrono::high_resolution_clock::now();
+
+  for (UnsafeUnion &u : unions) {
+    switch (u.tag) {
+    case UnionAType:
+      delete u.a;
+      break;
+    case UnionBType:
+      delete u.b;
+      break;
+    case UnionCType:
+      delete u.c;
+      break;
+    }
+  }
+
+  std::chrono::duration<double, std::milli> duration = t2 - t1;
+  cout << "unsafeUnionPerfTest: " << duration.count() << "ms\n"; 
+}
+
+
+struct Base {
+  virtual int foo(int n) = 0;
+  virtual ~Base() {};
+};
+struct SubtypeA : Base {
+  int foo(int n) override { return n + 1; }
+};
+struct SubtypeB : Base {
+  int foo(int n) override{ return n * 2; }
+};
+struct SubtypeC : Base {
+  int foo(int n) override { return n / 2; }
+};
+
+void subtypePerfTest(int num_objects)
+{
+  using std::vector;
+  using std::cout;
+  using std::endl;
+  std::vector<Base*> unions;
+
+  for (int i = 0; i < num_objects; ++i) {
+    if (i % 3 == 0) {
+      unions.push_back(new SubtypeA()); 
+    } else if (i % 3 == 1) {
+      unions.push_back(new SubtypeB()); 
+    } else {
+      unions.push_back(new SubtypeC()); 
+    }
+  }
+
+  auto t1 = std::chrono::high_resolution_clock::now();
+
+  int n = 1;
+  for (Base *b : unions) {
+    n += b->foo(n);
+  }
+
+  auto t2 = std::chrono::high_resolution_clock::now();
+
+  for (Base *b : unions) {
+    delete b;
+  }
+
+  std::chrono::duration<double, std::milli> duration = t2 - t1;
+  cout << "SubtypePerfTest: " << duration.count() << "ms\n"; 
+}
+
+
